63f97dc3b9caeaabbd8d3b4e4569f5fd08df0d35
Sergey Baturin <toko-nn@yandex.ru>
2023-01-23 15:10:36 +0300

    action build, lint, prettier8

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+export const ID_SECTION_FILTERS_TYPE_ROOM = "sectionSliderFiltersRoom";
+export const ID_SECTION_SELECT_LIGHT_CALCULATOR = "selectLightBrightness";
+export const ID_SECTION_SELECT_LAMP_CALCULATOR = "selectLamp";
+export const ID_SECTION_FILTERS_TYPE_HOUSE = "sectionSliderFiltersHouse";
+export const CLASS_BUTTON_HEART_ACTIVE: string = "card__button_active-heart";
+export const CLASS_SECTION_BLOG_BLOG: string = "blog_blog";
+
 type Grid = string;
 
 console.log("Hello, world!");
