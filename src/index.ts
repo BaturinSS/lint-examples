16c28ac6524a0b3fca2fe4ddbd8fa12b418a9b62
Sergey Baturin <toko-nn@yandex.ru>
2023-01-23 15:25:49 +0300

    action add cache

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ export const ID_SECTION_FILTERS_TYPE_ROOM = "sectionSliderFiltersRoom";
 export const ID_SECTION_SELECT_LIGHT_CALCULATOR = "selectLightBrightness";
 export const ID_SECTION_SELECT_LAMP_CALCULATOR = "selectLamp";
 export const ID_SECTION_FILTERS_TYPE_HOUSE = "sectionSliderFiltersHouse";
-export const CLASS_BUTTON_HEART_ACTIVE: string = "card__button_active-heart";
-export const CLASS_SECTION_BLOG_BLOG: string = "blog_blog";
-
-type Grid = string;
+export const CLASS_BUTTON_HEART_ACTIVE = "card__button_active-heart";
+export const CLASS_SECTION_BLOG_BLOG = "blog_blog";
 
 console.log("Hello, world!");
 
@@ -16,12 +14,3 @@ function greetName(name: string) {
 }
 
 greetName(Math.random() > 0.5 ? "Josh" : "anonymous");
-
-export const hello4: Grid = "world yay";
-
-const num = 10;
-const num = 10;
-
-const num = 10;
-
-console.log(num2, num);
